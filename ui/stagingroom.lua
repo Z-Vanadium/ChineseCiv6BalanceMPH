d9d9d03b03da1434cdc5ff55ba0c9c48ff938af3
z-vanadium <105497687@qq.com>
2025-10-15 21:33:20 +0800

    get mod property fix

--- ui/stagingroom.lua
+++ ui/stagingroom.lua
@@ -572,8 +572,8 @@ function GetLocalModVersion(id)
 	end
 	if handle ~= -1 then
 		local version = Modding.GetModProperty(handle, "version");
-		-- version = (version == nil and Modding.GetModProperty(handle, "version"))
-		-- version = (version == nil and Modding.GetModProperty(handle, "VERSION"))
+		version = (version == nil and Modding.GetModProperty(handle, "Version"))
+		version = (version == nil and Modding.GetModProperty(handle, "VERSION"))
 		return version
 		else
 		return nil
